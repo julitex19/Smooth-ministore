e5ae7e618a50fedeb1657d2344335fa9f6c5eee7
Juliet Samuel <julitex19@gmail.com>
2024-07-15 22:13:49 +0100

    corrections

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,3 +1,139 @@
+// import Link from 'next/link';
+// import Image from 'next/image';
+// import qrcode from '../../public/qr.svg';
+// import playstore from '../../public/googleplay.svg';
+// import appstore from '../../public/AppStore.svg';
+// import { FaGithub, FaInstagram } from 'react-icons/fa';
+// import { FaXTwitter } from 'react-icons/fa6';
+// import { LuLinkedin } from 'react-icons/lu';
+// import { VscSend } from "react-icons/vsc";
+
+
+// const Footer = () => {
+//   const socialLinks = [
+//     { icon: <FaGithub />, url: "https://github.com/julitex19" },
+//     { icon: <FaXTwitter />, url: "http://linkedin.com/in/julietobi" },
+//     { icon: <LuLinkedin />, url: "http://linkedin.com/in/julietobi" },
+//     { icon: <FaInstagram />, url: "http://linkedin.com/in/julietobi" },
+//   ];
+
+//   return (
+//     <footer className="bg-black mt-24">
+//       <div className="container mx-auto px-4 py-8 flex flex-wrap md:justify-around gap-8 text-white">
+
+//         {/* Exclusive Section */}
+//         <div className="flex flex-col">
+//           <h3 className="text-2xl font-normal">Exclusive</h3>
+//           <ul className="text-sm">
+//             <li className="py-2"><a href="/">Subscribe</a></li>
+//             <li className="py-2"><a href="/">Get 10% off your first order</a></li>
+//           </ul>
+//           <div className="relative mt-4">
+//             <input
+//               type="email"
+//               className="p-3 border border-white rounded-md w-56 bg-transparent text-white text-sm"
+//               placeholder="Enter Your Email?"
+//             />
+//             <button className="absolute top-3 right-3 bg-black cursor-pointer">
+//               <VscSend size={26}/>
+//             </button>
+//           </div>
+//         </div>
+
+//         {/* Support Section */}
+//         <div className="">
+//           <h4 className="text-xl font-normal">Support</h4>
+//           <p className="text-base font-light py-2 ">120, Off Cresent way Alagbaka,<br /> Akure, Ondo State, NIgeria.</p>
+//           <a href="/" className="text-base font-light">exclusive@gmail.com</a>
+//           <p className="text-base font-light py-2">+88015-88888-9999</p>
+//         </div>
+
+//         {/* Account Section */}
+//         <div className="text-white">
+//           <h4 className="text-xl font-normal">Account</h4>
+//           <ul className="mt-2 text-sm">
+//             <li className="py-2">
+//               <Link href="/account" legacyBehavior>
+//                 <a>My Account</a>
+//               </Link>
+//             </li>
+//             <li className="py-2">
+//               <Link href="/login" legacyBehavior>
+//                 <a>Login / Register</a>
+//               </Link>
+//             </li>
+//             <li className="py-2">
+//               <Link href="/cart" legacyBehavior>
+//                 <a>Cart</a>
+//               </Link>
+//             </li>
+//             <li className="py-2">
+//               <Link href="/wishlist" legacyBehavior>
+//                 <a>Wishlist</a>
+//               </Link>
+//             </li>
+//             <li className="py-2">
+//               <Link href="/shop" legacyBehavior>
+//                 <a>Shop</a>
+//               </Link>
+//             </li>
+//           </ul>
+//         </div>
+
+//         {/* Quick Link Section */}
+//         <div className="text-white">
+//           <h4 className="text-xl font-normal">Quick link</h4>
+//           <ul className="mt-2 text-sm">
+//             <li className="py-2"><a href="/">Privacy Policy</a></li>
+//             <li className="py-2"><a href="/">Terms Of Use</a></li>
+//             <li className="py-2"><a href="/">FAQ</a></li>
+//             <li className="py-2"><a href="/">Contact</a></li>
+//           </ul>
+//         </div>
+
+//         {/* Download App Section */}
+//         <div className="text-white">
+//           <h3 className="text-xl font-normal">Download App</h3>
+//           <span className="text-gray-400 text-sm font-light block mt-2">Save $3 with App New User Only</span>
+//           <div className="flex items-center gap-2 mt-4">
+//             <div className="w-18 h-18">
+//               <Image src={qrcode} alt="Qrcode" />
+//             </div>
+//             <div className="flex flex-col gap-2">
+//               <Image src={playstore} alt={"Google Play"} width={115} height={40} />
+//               <Image src={appstore} alt={"App store"} width={118} height={40} />
+//             </div>
+//           </div>
+
+//           <div className="pt-3">
+//             <div className="flex items-center gap-5">
+//                 {socialLinks.map((social, index) => (
+//                   <a
+//                     key={index}
+//                     href={social.url}
+//                     target="_blank"
+//                     rel="noopener noreferrer"
+//                     className="text-[1.2rem] text-brandSecondary dark:text-brandGray2 dark:hover:text-darkPrimary hover:text-brandPrimary cursor-pointer hover:transform hover:-translate-y-1 transition-all duration-100"
+//                   >
+//                     {social.icon}
+//                   </a>
+//                 ))}
+//               </div>
+           
+//           </div>
+//         </div>
+//       </div>
+//       <div className="flex justify-center items-center gap-2 py-4 border-t border-gray-800">
+//         <p className="text-gray-400 text-sm font-light">Copyright Juliet HNG ©2024 All right reserved</p>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
+
 import Link from 'next/link';
 import Image from 'next/image';
 import qrcode from '../../public/qr.svg';
@@ -6,21 +142,19 @@ import appstore from '../../public/AppStore.svg';
 import { FaGithub, FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
 import { LuLinkedin } from 'react-icons/lu';
-import { VscSend } from "react-icons/vsc";
-
+import { VscSend } from 'react-icons/vsc';
 
 const Footer = () => {
   const socialLinks = [
-    { icon: <FaGithub />, url: "https://github.com/julitex19" },
-    { icon: <FaXTwitter />, url: "http://linkedin.com/in/julietobi" },
-    { icon: <LuLinkedin />, url: "http://linkedin.com/in/julietobi" },
-    { icon: <FaInstagram />, url: "http://linkedin.com/in/julietobi" },
+    { icon: <FaGithub />, url: 'https://github.com/julitex19' },
+    { icon: <FaXTwitter />, url: 'http://linkedin.com/in/julietobi' },
+    { icon: <LuLinkedin />, url: 'http://linkedin.com/in/julietobi' },
+    { icon: <FaInstagram />, url: 'http://linkedin.com/in/julietobi' },
   ];
 
   return (
     <footer className="bg-black mt-24">
       <div className="container mx-auto px-4 py-8 flex flex-wrap md:justify-around gap-8 text-white">
-
         {/* Exclusive Section */}
         <div className="flex flex-col">
           <h3 className="text-2xl font-normal">Exclusive</h3>
@@ -35,15 +169,19 @@ const Footer = () => {
               placeholder="Enter Your Email?"
             />
             <button className="absolute top-3 right-3 bg-black cursor-pointer">
-              <VscSend size={26}/>
+              <VscSend size={26} />
             </button>
           </div>
         </div>
 
         {/* Support Section */}
-        <div className="">
+        <div>
           <h4 className="text-xl font-normal">Support</h4>
-          <p className="text-base font-light py-2 ">120, Off Cresent way Alagbaka,<br /> Akure, Ondo State, NIgeria.</p>
+          <p className="text-base font-light py-2">
+            120, Off Crescent way Alagbaka,
+            <br />
+            Akure, Ondo State, Nigeria.
+          </p>
           <a href="/" className="text-base font-light">exclusive@gmail.com</a>
           <p className="text-base font-light py-2">+88015-88888-9999</p>
         </div>
@@ -52,31 +190,13 @@ const Footer = () => {
         <div className="text-white">
           <h4 className="text-xl font-normal">Account</h4>
           <ul className="mt-2 text-sm">
-            <li className="py-2">
-              <Link href="/account" legacyBehavior>
-                <a>My Account</a>
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/login" legacyBehavior>
-                <a>Login / Register</a>
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/cart" legacyBehavior>
-                <a>Cart</a>
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/wishlist" legacyBehavior>
-                <a>Wishlist</a>
-              </Link>
-            </li>
-            <li className="py-2">
-              <Link href="/shop" legacyBehavior>
-                <a>Shop</a>
-              </Link>
-            </li>
+            {['My Account', 'Login / Register', 'Cart', 'Wishlist', 'Shop'].map((item) => (
+              <li className="py-2" key={item}>
+                <Link href={`/${item.toLowerCase().replace(/ /g, '')}`} legacyBehavior>
+                  <a>{item}</a>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
 
@@ -84,50 +204,51 @@ const Footer = () => {
         <div className="text-white">
           <h4 className="text-xl font-normal">Quick link</h4>
           <ul className="mt-2 text-sm">
-            <li className="py-2"><a href="/">Privacy Policy</a></li>
-            <li className="py-2"><a href="/">Terms Of Use</a></li>
-            <li className="py-2"><a href="/">FAQ</a></li>
-            <li className="py-2"><a href="/">Contact</a></li>
+            {['Privacy Policy', 'Terms Of Use', 'FAQ', 'Contact'].map((item) => (
+              <li className="py-2" key={item}><a href="/">{item}</a></li>
+            ))}
           </ul>
         </div>
 
         {/* Download App Section */}
         <div className="text-white">
           <h3 className="text-xl font-normal">Download App</h3>
-          <span className="text-gray-400 text-sm font-light block mt-2">Save $3 with App New User Only</span>
+          <span className="text-gray-400 text-sm font-light block mt-2">
+            Save $3 with App New User Only
+          </span>
           <div className="flex items-center gap-2 mt-4">
             <div className="w-18 h-18">
               <Image src={qrcode} alt="Qrcode" />
             </div>
             <div className="flex flex-col gap-2">
-              <Image src={playstore} alt={"Google Play"} width={115} height={40} />
-              <Image src={appstore} alt={"App store"} width={118} height={40} />
+              <Image src={playstore} alt="Google Play" width={115} height={40} />
+              <Image src={appstore} alt="App store" width={118} height={40} />
             </div>
           </div>
-
           <div className="pt-3">
             <div className="flex items-center gap-5">
-                {socialLinks.map((social, index) => (
-                  <a
-                    key={index}
-                    href={social.url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-[1.2rem] text-brandSecondary dark:text-brandGray2 dark:hover:text-darkPrimary hover:text-brandPrimary cursor-pointer hover:transform hover:-translate-y-1 transition-all duration-100"
-                  >
-                    {social.icon}
-                  </a>
-                ))}
-              </div>
-           
+              {socialLinks.map((social, index) => (
+                <a
+                  key={index}
+                  href={social.url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-[1.2rem] hover:text-brandPrimary cursor-pointer hover:transform hover:-translate-y-1 transition-all duration-100"
+                >
+                  {social.icon}
+                </a>
+              ))}
+            </div>
           </div>
         </div>
       </div>
       <div className="flex justify-center items-center gap-2 py-4 border-t border-gray-800">
-        <p className="text-gray-400 text-sm font-light">Copyright Juliet HNG ©2024 All right reserved</p>
+        <p className="text-gray-400 text-sm font-light">
+          Copyright Juliet HNG ©2024 All right reserved
+        </p>
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,3 +1,157 @@
+// "use client";
+// import React, { useContext, useMemo, useState } from "react";
+// import Link from "next/link";
+// import { CiHeart, CiMenuFries } from "react-icons/ci";
+// import { PiShoppingCartSimpleThin } from "react-icons/pi";
+// import { IoIosAdd } from "react-icons/io";
+// import { LiaTimesSolid } from "react-icons/lia";
+// import Advert from "./Advert";
+// import { StoreProvider } from "@/Providers/Context";
+
+// const Navbar: React.FC = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+//   const {
+//     state: { cart },
+//   } = useContext(StoreProvider);
+
+//   const toggleMenu = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   const totalQuantity = useMemo(() => {
+//     return cart.reduce((total: any, el: { qty: any; }) => total + el.qty, 0);
+//   }, [cart]);
+
+//   console.log(totalQuantity);
+
+//   return (
+//     <div className="fixed top-0 w-full z-50 shadow">
+//       <Advert />
+//       <nav className="bg-white ">
+//         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-11 py-2">
+//           <div className="flex items-center justify-between h-16 ">
+//             <div className="flex items-center gap-20">
+//               <div className="flex-shrink-0">
+//                 <h1 className="text-black text-xl font-bold">Juliet</h1>
+//               </div>
+//               <div className="hidden md:block">
+//                 <div className="ml-10 flex items-baseline space-x-4">
+//                   <Link
+//                     href="/"
+//                     className="text-black hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+//                   >
+//                     Home
+//                   </Link>
+//                   <Link
+//                     href="/about"
+//                     className="text-black hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+//                   >
+//                     About
+//                   </Link>
+//                   <Link
+//                     href="/contact"
+//                     className="text-black hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+//                   >
+//                     Contact
+//                   </Link>
+//                   <Link
+//                     href="/signup"
+//                     className="text-black hover:bg-gray-100 px-3 py-2 rounded-md text-sm font-medium"
+//                   >
+//                     Signup
+//                   </Link>
+//                 </div>
+//               </div>
+//             </div>
+//             <div className="hidden md:flex md:items-center md:space-x-8">
+//               <input
+//                 type="text"
+//                 className="px-3 py-2 rounded-md text-sm bg-gray-100 text-black focus:outline-none"
+//                 placeholder="Search..."
+//               />
+//               <CiHeart size="32" />
+//               <Link href="/cart" className="relative">
+//                 <span className="absolute -right-[4px] top-[1px] rounded-full w-4 h-4 text-xs bg-red-600 text-white flex justify-center items-center">
+//                   {totalQuantity}
+//                 </span>
+//                 <PiShoppingCartSimpleThin size="32" />
+//               </Link>
+//               <IoIosAdd size="32" />
+//             </div>
+//             <div className="-mr-2 flex md:hidden">
+//               <button
+//                 onClick={toggleMenu}
+//                 type="button"
+//                 className=" inline-flex items-center justify-center p-2 rounded-md text-black  focus:outline-none focus:ring-2 focus:ring-offset-2  focus:ring-white"
+//                 aria-controls="mobile-menu"
+//                 aria-expanded={isOpen}
+//               >
+//                 <span className="sr-only">Open main menu</span>
+//                 {isOpen ? (
+//                   <LiaTimesSolid size="32" />
+//                 ) : (
+//                   <CiMenuFries size="32" />
+//                 )}
+//               </button>
+//             </div>
+//           </div>
+//         </div>
+
+//         <div
+//           className={`${isOpen ? "block" : "hidden"} md:hidden`}
+//           id="mobile-menu"
+//         >
+//           <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+//             <Link
+//               href="/"
+//               className="text-black hover:bg-gray-100 block px-3 py-2 rounded-md text-base font-medium"
+//             >
+//               Home
+//             </Link>
+//             <Link
+//               href="/about"
+//               className="text-black hover:bg-gray-100 block px-3 py-2 rounded-md text-base font-medium"
+//             >
+//               About
+//             </Link>
+//             <Link
+//               href="/contact"
+//               className="text-black hover:bg-gray-100 block px-3 py-2 rounded-md text-base font-medium"
+//             >
+//               Contact
+//             </Link>
+//             <Link
+//               href="/signup"
+//               className="text-black hover:bg-gray-100 block px-3 py-2 rounded-md text-base font-medium"
+//             >
+//               Signup
+//             </Link>
+//             <input
+//               type="text"
+//               className="px-3 py-2 rounded-md text-sm bg-gray-100 text-black w-full focus:outline-none"
+//               placeholder="Search..."
+//             />
+//             <div className="flex justify-around py-2">
+//               <CiHeart size="32" />
+//               <div className="relative">
+//                 <span className="absolute -right-[4px] top-[1px] rounded-full w-4 h-4 text-xs bg-red-600 text-white flex justify-center items-center">
+//                   1
+//                 </span>
+//                 <PiShoppingCartSimpleThin size="32" />
+//               </div>
+//               <IoIosAdd size="32" />
+//             </div>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
 "use client";
 import React, { useContext, useMemo, useState } from "react";
 import Link from "next/link";
@@ -19,10 +173,8 @@ const Navbar: React.FC = () => {
   };
 
   const totalQuantity = useMemo(() => {
-    return cart.reduce((total: any, el: { qty: any; }) => total + el.qty, 0);
+    return cart.reduce((total, el) => total + el.qty, 0);
   }, [cart]);
-
-  console.log(totalQuantity);
 
   return (
     <div className="fixed top-0 w-full z-50 shadow">
@@ -149,3 +301,4 @@ const Navbar: React.FC = () => {
 };
 
 export default Navbar;
+
